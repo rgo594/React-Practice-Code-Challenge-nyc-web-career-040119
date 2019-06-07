23fa1edbfec86a8550e4e0976b733917b187af3e
Robert O'Gara <rgo594@gmail.com>
2019-06-07 16:00:24 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,42 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    sushi: []
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => this.setState({ sushi: data }));
+    }
+
+    sushiList = () => {
+      return this.state.sushi.map( soosh => {
+        return soosh.name
+      }).splice(0, 4)
+    }
+    //I probably couldve done some of the other deliverables, but it makes it really hard to test anything without this
+
+    moreBtn = (e) => {
+      e.target
+    }
+
+    // deleteSushi = (e) => {
+    //   if (e.currentTarget === )
+    // }
+
 
   render() {
+    console.log(this.state.sushi.map( soosh => {
+      return soosh.name
+    }).splice(0, 4))
     return (
       <div className="app">
-        <SushiContainer  />
+      <li>{this.sushiList}</li>
+        <SushiContainer btnClick={this.moreBtn}/>
         <Table />
+
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,24 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
 
-const SushiContainer = (props) => {
+class SushiContainer extends React.Component {
+
+  // sushiList = () => {
+  //   return this.state.sushi.map( soosh => {
+  //     return <li>{soosh.name}</li>
+  //   })
+  // }
+
+  render(){
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
+
         <MoreButton />
       </div>
     </Fragment>
   )
+  }
 }
 
 export default SushiContainer
